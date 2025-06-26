6eff3ef4fcb1dc62ebb44866555e38b875a3d607
dp2ptrade <zxoomxy@gmail.com>
2025-06-26 11:25:57 +0600

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,6 +14,7 @@ import { UserService } from '../lib/supabaseService';
 import AppSidebar from '../components/AppSidebar';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { Drawer } from 'react-native-drawer-layout'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 const theme = {
   ...DefaultTheme,
